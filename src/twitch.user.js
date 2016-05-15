22a88c585b365f68b1183456ff802746d7de3723
JIeTKa <dyus6661@gmail.com>
2016-05-15 21:07:22 +0300

    Updated twitch.user.js file

--- src/twitch.user.js
+++ src/twitch.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
+// @name TwitchChatHelper
+// @namespace twitchUtils
 // @description Twitch chat helper
-// 
+// @include http://twitch.tv/*
+// @include https://twitch.tv/*
+// @updateURL https://github.com/JIeTKa/TwitchChatHelper/raw/master/dist/twitch.meta.js
+// @downloadURL https://github.com/JIeTKa/TwitchChatHelper/raw/master/dist/twitch.user.js
+// @version 1.0
+// @grant none
 // ==/UserScript==
-
-var test = function(){
-
-	return console.log("Hello World!");
-}
